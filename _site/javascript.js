be3a1dd5091430d5c04d4cf3d6c207c5337a5e35
Steven Leiva <leiva.steven@gmail.com>
2023-07-12 22:46:08 -0500

    Add a "Print" button to Cert 1 that hides the spanish version.
    This is a smoke test to make sure everything is working.

---
+++ _site/javascript.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  function printSingleCertificate(event) {
+    const currentCertificate = event.target.closest('.certificate');
+
+    // Hide all other certificates from the print view
+    document.querySelectorAll('.certificate').forEach(certificate => {
+      if (certificate !== currentCertificate) {
+        certificate.classList.add('-no-print');
+      }
+    })
+
+    window.print()
+  }
+
+  // Grab all "Print" buttons and add an event listener
+  document.querySelector('.cert-header button').addEventListener('click', printSingleCertificate);
+
+  // Always undo the work of `printSingleCertificate` when the print dialog is closed
+  window.addEventListener('afterprint', () => {
+    document.querySelectorAll('.certificate').forEach(certificate => {
+        certificate.classList.remove('-no-print');
+    })
+  })
+});
