454f68209a1d36fc8669ebc98e328329a3a0d52e
Restyled.io <commits@restyled.io>
2023-07-14 20:54:29 +0000

    Restyled by prettier

--- _site/javascript.js
+++ _site/javascript.js
@@ -1,26 +1,27 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   function printSingleCertificate(event) {
-    const currentCertificate = event.target.closest('.certificate');
+    const currentCertificate = event.target.closest(".certificate");
 
     // Hide all other certificates from the print view
-    document.querySelectorAll('.certificate').forEach(certificate => {
+    document.querySelectorAll(".certificate").forEach((certificate) => {
       if (certificate !== currentCertificate) {
-        certificate.classList.add('-no-print');
+        certificate.classList.add("-no-print");
       }
-    })
+    });
 
-    window.print()
+    window.print();
   }
 
   // Grab all "Print" buttons and add an event listener
-  document.querySelectorAll('.cert-header button')
-      .forEach(el => el.addEventListener('click', printSingleCertificate));
+  document
+    .querySelectorAll(".cert-header button")
+    .forEach((el) => el.addEventListener("click", printSingleCertificate));
 
   // Always undo the work of `printSingleCertificate` when the print dialog is
   // closed
-  window.addEventListener(
-      'afterprint',
-      () => {document.querySelectorAll('.certificate').forEach(certificate => {
-        certificate.classList.remove('-no-print');
-      })})
+  window.addEventListener("afterprint", () => {
+    document.querySelectorAll(".certificate").forEach((certificate) => {
+      certificate.classList.remove("-no-print");
+    });
+  });
 });
