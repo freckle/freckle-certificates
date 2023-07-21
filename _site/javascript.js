4cfc028a5e060a4eaf846f7c0c15be4b334c0341
Jason Lieb <Jason.lieb@renaissance.com>
2023-07-21 11:31:39 -0400

    formatting and remove unnecessary !important

--- _site/javascript.js
+++ _site/javascript.js
@@ -3,35 +3,35 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
   /* Hide all certificates except the one that was clicked */
   function printSingleCertificate(event) {
-    const currentCertificate = event.target.closest('.certificate');
+    const currentCertificate = event.target.closest('.certificate')
 
     document.querySelectorAll('.certificate').forEach((certificate) => {
       if (certificate !== currentCertificate) {
-        certificate.classList.add('-no-print');
+        certificate.classList.add('-no-print')
       } else {
-        certificate.classList.add('safari-print-single');
+        certificate.classList.add('safari-print-single')
       }
-    });
+    })
 
-    window.print();
+    window.print()
   }
 
   /* Add a note to Safari users */
   function addSafariNote() {
-    const userAgent = navigator.userAgent;
+    const userAgent = navigator.userAgent
 
     /* Please see table: https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#browser_name_and_version */
-    const containsSafari = userAgent.includes('Safari');
-    const containsChrome = userAgent.includes('Chrome');
-    const containsChromium = userAgent.includes('Chromium');
+    const containsSafari = userAgent.includes('Safari')
+    const containsChrome = userAgent.includes('Chrome')
+    const containsChromium = userAgent.includes('Chromium')
 
-    const isSafari = containsSafari && !containsChrome && !containsChromium;
+    const isSafari = containsSafari && !containsChrome && !containsChromium
 
     if (isSafari) {
-      document.querySelector('#safari-note').classList.remove('-hidden');
+      document.querySelector('#safari-note').classList.remove('-hidden')
       document
         .querySelectorAll('.cert-frame')
-        .forEach((el) => el.classList.add('safari'));
+        .forEach((el) => el.classList.add('safari'))
     }
   }
 
@@ -42,16 +42,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
   /* Grab all "Print" buttons and add an event listener */
   document
     .querySelectorAll('.cert-header button')
-    .forEach((el) => el.addEventListener('click', printSingleCertificate));
+    .forEach((el) => el.addEventListener('click', printSingleCertificate))
 
   /* Undo the hiding of certificates after printing */
   window.addEventListener('afterprint', () => {
     document.querySelectorAll('.certificate').forEach((certificate) => {
-      certificate.classList.remove('-no-print');
-      certificate.classList.remove('safari-print-single');
-    });
-  });
+      certificate.classList.remove('-no-print')
+      certificate.classList.remove('safari-print-single')
+    })
+  })
 
   /* Add a note to Safari users */
-  addSafariNote();
-});
+  addSafariNote()
+})
