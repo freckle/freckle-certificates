ddefb1cc2fb48115a080b7e788efc2232dd9f69e
Jason Lieb <Jason.lieb@renaissance.com>
2023-07-21 00:00:57 -0400

    create safari classes for printing

--- _site/javascript.js
+++ _site/javascript.js
@@ -1,17 +1,19 @@
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {
   /* FUNCTION DECLARATIONS */
 
   /* Hide all certificates except the one that was clicked */
   function printSingleCertificate(event) {
     const currentCertificate = event.target.closest('.certificate');
 
-    document.querySelectorAll('.certificate').forEach(certificate => {
+    document.querySelectorAll('.certificate').forEach((certificate) => {
       if (certificate !== currentCertificate) {
         certificate.classList.add('-no-print');
+      } else {
+        certificate.classList.add('safari-print-single');
       }
-    })
+    });
 
-    window.print()
+    window.print();
   }
 
   /* Add a note to Safari users */
@@ -23,10 +25,13 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const containsChrome = userAgent.includes('Chrome');
     const containsChromium = userAgent.includes('Chromium');
 
-    const isSafari = containsSafari && !containsChrome && !containsChromium
+    const isSafari = containsSafari && !containsChrome && !containsChromium;
 
     if (isSafari) {
       document.querySelector('#safari-note').classList.remove('-hidden');
+      document
+        .querySelectorAll('.cert-frame')
+        .forEach((el) => el.classList.add('safari'));
     }
   }
 
@@ -35,16 +40,17 @@ window.addEventListener("DOMContentLoaded", (event) => {
   /* FUNCTION CALLS */
 
   /* Grab all "Print" buttons and add an event listener */
-  document.querySelectorAll('.cert-header button').forEach(el =>
-    el.addEventListener('click', printSingleCertificate)
-  ) ;
+  document
+    .querySelectorAll('.cert-header button')
+    .forEach((el) => el.addEventListener('click', printSingleCertificate));
 
   /* Undo the hiding of certificates after printing */
   window.addEventListener('afterprint', () => {
-    document.querySelectorAll('.certificate').forEach(certificate => {
-        certificate.classList.remove('-no-print');
-    })
-  })
+    document.querySelectorAll('.certificate').forEach((certificate) => {
+      certificate.classList.remove('-no-print');
+      certificate.classList.remove('safari-print-single');
+    });
+  });
 
   /* Add a note to Safari users */
   addSafariNote();
